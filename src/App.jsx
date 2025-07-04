1833b332639225ddbdb87ecfd8a70fea32541dc1
JocelynTPA <jocelyn@grayfordcapital.com>
2025-07-04 19:19:44 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to 80health</h1>
+      <p>This is your React landing page.</p>
+    </div>
+  );
+}
+
+export default App;
