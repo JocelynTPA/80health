eb58e4cb5b62d94c446e5968a703d169f551498a
JocelynTPA <jocelyn@grayfordcapital.com>
2025-07-05 19:19:46 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html"
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
     extend: {
       colors: {
-        charcoal: '#1A1C1D',
-        gold: '#B5A581'
-      }
-    }
+        charcoal: '#1A1C1D',         // primary
+        gold: '#B5A581',             // metallic accent
+        burgundy: '#6F2C3F',         // refined highlight
+        cream: '#F5F4F3',            // light neutral
+        white: '#FFFFFF'             // pristine white
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 }
